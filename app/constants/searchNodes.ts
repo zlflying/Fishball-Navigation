293557f002abad67655b4ea916dde5cc7ec8ea01
zlflying <zlflying@qq.com>
2025-09-21 09:08:49 +0800

    add icon

--- app/constants/searchNodes.ts
+++ app/constants/searchNodes.ts
@@ -1,7 +1,3 @@
-import {getEnvConfig} from "~~/env/env";
-
-const envConfig = getEnvConfig();
-
 /** 搜索节点 */
 export interface SearchNode {
     /** 代码 */
@@ -19,37 +15,37 @@ const searchNodes: SearchNode[] = [
     {
         sourceCode: "bing",
         sourceName: "必应",
-        iconPath: envConfig.baseUrl + "img/search/bing.png",
+        iconPath: "./img/search/bing.png",
         baseUrl: "https://www.bing.com/search?q="
     },
     {
         sourceCode: "wanfang",
         sourceName: "万方",
-        iconPath: envConfig.baseUrl + "img/search/wanfang.png",
+        iconPath: "./img/search/wanfang.png",
         baseUrl: "https://s.wanfangdata.com.cn/paper?q="
     },
     {
         sourceCode: "baidu",
         sourceName: "百度学术",
-        iconPath: envConfig.baseUrl + "img/search/baidu.png",
+        iconPath: "./img/search/baidu.png",
         baseUrl: "https://xueshu.baidu.com/s?wd="
     },
     {
         sourceCode: "pubmed",
         sourceName: "PubMed",
-        iconPath: envConfig.baseUrl + "img/search/pubmed.png",
+        iconPath: "./img/search/pubmed.png",
         baseUrl: "https://pubmed.ncbi.nlm.nih.gov/?term="
     },
     {
         sourceCode: "github",
         sourceName: "GitHub",
-        iconPath: envConfig.baseUrl + "img/search/github.png",
+        iconPath: "./img/search/github.png",
         baseUrl: "https://github.com/search?q="
     },
     {
         sourceCode: "translator",
         sourceName: "在线翻译",
-        iconPath: envConfig.baseUrl + "img/search/translator.png",
+        iconPath: "./img/search/translator.png",
         baseUrl: "https://www.deepl.com/zh/translator#zh/en-us/"
     }
 ];
