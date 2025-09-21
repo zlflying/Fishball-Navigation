fb3eb831a22478136bbd8be01d72113b856f28e4
zlflying <zlflying@qq.com>
2025-09-22 00:20:17 +0800

    update

--- app/components/ContentItem.vue
+++ app/components/ContentItem.vue
@@ -11,14 +11,22 @@ const props = defineProps({
 
 <template>
   <div>
-    <div class="title">{{ props.node.displayName }}</div>
+    <div class="title tw-mb-2 tw-text-base tw-font-medium">{{ props.node.displayName }}</div>
     <div class="tw-flex tw-gap-4 tw-flex-wrap">
-      <div class="item tw-bg-white dark:tw-border-gray-500 hover:tw-translate-y-[-1] dark:hover:tw-bg-gray-700 tw-w-80 tw-pl-3 tw-pr-3 tw-pt-5 tw-pb-5 tw-rounded-xl" v-for="item in props.node.items">
+      <div
+          class="item tw-cursor-pointer tw-transition-all
+          tw-bg-white dark:tw-bg-gray-500 dark:hover:tw-bg-gray-700
+          tw-w-80 tw-pl-3 tw-pr-3 tw-pt-5 tw-pb-5 tw-rounded-xl
+          hover:tw-translate-y-[-5px] hover:tw-ease hover:tw-duration-500 hover:tw-shadow-2xl
+          "
+          v-for="item in props.node.items">
         <div class="tw-flex tw-items-center tw-gap-2 tw-h-full">
           <img src="/img/logo.svg" alt="" class="tw-rounded-full tw-w-12 tw-h-12">
-          <div class="tw-flex tw-flex-col tw-gap-2">
+          <div class="tw-flex tw-flex-col tw-gap-2 tw-w-[calc(100%-48px-0.5rem)]">
             <div class="tw-font-sans tw-text-sm">{{ item.displayName }}</div>
-            <div class="tw-text-xs tw-text-slate-400 tw-text-nowrap tw-overflow-hidden tw-text-ellipsis">{{ item.description }}</div>
+            <div class="tw-text-xs tw-text-slate-400 tw-truncate">
+              {{ item.description }}
+            </div>
           </div>
         </div>
       </div>
